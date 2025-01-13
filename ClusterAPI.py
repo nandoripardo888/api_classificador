8e964f61594ff6fdca1a41bf734d7a92630d20f9
nandoripardo888 <49971513+nandoripardo888@users.noreply.github.com>
2025-01-13 13:05:35 -0400

    Update ClusterAPI.py

--- ClusterAPI.py
+++ ClusterAPI.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Importar CORS
 import json
+import os
 from Clusterizador import Clusterizador
+
 class ClusterAPI:
     def __init__(self):
         self.app = Flask(__name__)
@@ -33,8 +35,14 @@ class ClusterAPI:
     def run(self, debug=False):
         self.app.run(debug=debug)
 
-# Inicialização do servidor
-if __name__ == "__main__":
-    api = ClusterAPI()
-    api.clusterizador.initialize_from_file(r"C:\Users\Pichau\Documents\python_estudo\classificador\dados_cluster.json")
-    api.run(debug=True)
+# Criar uma instância da API e expor o objeto `app` para o Gunicorn
+api = ClusterAPI()
+
+#api.clusterizador.initialize_from_file(r"C:\Users\Pichau\Documents\python_estudo\classificador\dados_cluster.json")
+# Caminho relativo para o arquivo
+file_path = os.path.join(os.path.dirname(__file__), "dados_cluster.json")
+
+# Inicialização
+api.clusterizador.initialize_from_file(file_path)
+
+app = api.app
